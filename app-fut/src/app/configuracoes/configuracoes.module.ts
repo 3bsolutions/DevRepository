77fbd37e0469b5b46d143ad491ea111e2e662c1e
fer-nan-do <nandinho_sbo@hotmail.com>
2019-04-22 23:48:36 -0300

    app-sorteio-times

---
+++ app-fut/src/app/configuracoes/configuracoes.module.ts
@@ -0,0 +1,16 @@
+import { ConfiguracoesComponent } from './configuracoes.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    ConfiguracoesComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ConfiguracoesComponent
+  ]
+})
+export class ConfiguracoesModule { }
